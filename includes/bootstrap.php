7b24612925731af5acee164c1a29d2fb6cf0f40e
Filip Procházka <filip@prochazka.su>
2016-01-21 23:58:30 +0100

    custom InfiniteArray

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -10,3 +10,55 @@ call_user_func(function () {
     session_set_cookie_params(60 * 60 * 24 * 14, '/', '.' . $host['domain']);
 });
 
+
+
+class InfiniteArray implements ArrayAccess
+{
+
+    /**
+     * @var mixed
+     */
+    private $value;
+
+    /**
+     * @var array
+     */
+    private $override;
+
+
+
+    public function __construct($value, array $override = [])
+    {
+        $this->value = $value;
+        $this->override = $override;
+    }
+
+
+
+    public function offsetExists($offset)
+    {
+        return true;
+    }
+
+
+
+    public function offsetGet($offset)
+    {
+        return array_key_exists($offset, $this->override) ? $this->override[$offset] : $this->value;
+    }
+
+
+
+    public function offsetSet($offset, $value)
+    {
+        $this->override[$offset] = $value;
+    }
+
+
+
+    public function offsetUnset($offset)
+    {
+        unset($this->override[$offset]);
+    }
+
+}

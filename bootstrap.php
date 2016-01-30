681e639d7d9fa130c8c149412f3e3c5ea13ff441
Filip Procházka <filip@prochazka.su>
2016-01-30 17:28:19 +0100

    domain whitelist

--- bootstrap.php
+++ bootstrap.php
@@ -9,8 +9,22 @@ require_once __DIR__ . '/vendor/autoload.php';
 call_user_func(function () {
     Debugger::enable(Debugger::PRODUCTION, __DIR__ . '/logs');
 
+    $domainWhitelist = [
+        'fi.muwiki',
+    ];
+
     $httpRequest = (new RequestFactory())->createHttpRequest();
-    if (preg_match('~^(?:.*\.)?(?P<domain>\w+\.\w+)$~', $httpRequest->getUrl()->getHost(), $host)) {
+    if (preg_match('~^(?:(?P<second>.+)\.)?(?P<first>\w+)\.(?P<tld>\w+)$~', $httpRequest->getUrl()->getHost(), $host)) {
+        $host['domain'] = $host['first'] . $host['tld'];
+        $host['withoutTld'] = (isset($host['second']) ? $host['second'] . '.' : '') . $host['first'];
+
+        if (PHP_SAPI !== 'cli' && !in_array($host['withoutTld'], $domainWhitelist, true)) {
+            $response = new \Nette\Http\Response();
+            $response->setCode($response::S404_NOT_FOUND);
+            require_once __DIR__ . '/public/4xx/404.html';
+            exit;
+        }
+
         session_set_cookie_params(60 * 60 * 24 * 14, '/', '.' . $host['domain'], TRUE, TRUE);
     }
     ini_set('session.save_path', __DIR__ . '/temp');
